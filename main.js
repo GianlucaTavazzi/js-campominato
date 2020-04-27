c83830795209a5aefd07ad4e7b3d28db1c250b6f
Gianluca Tavazzi <gianluca.tavazzi@gmail.com>
2020-04-27 23:45:07 +0200

    fine

---
+++ main.js
@@ -0,0 +1,36 @@
+//generare 16 numeri casuali
+var arr = [];
+
+var numero_casuale;
+
+while (arr.length < 16) {
+    numero_casuale = Math.floor(Math.random() * 100) + 1;
+    if (!arr.includes(numero_casuale)) {
+        arr.push(numero_casuale)
+    }
+}
+
+console.log(arr);
+
+var arr_utente = []
+
+var numero_utente;
+
+do {
+    //chiedere all'utente di inserire un numero
+    var numero_utente = parseInt(prompt('inserisci un numero'))
+    console.log(numero_utente);
+    if ((numero_utente <= 100) && (numero_utente >= 1) && !arr_utente.includes(numero_utente)) {
+        console.log('Bravo, continua così!');
+        arr_utente.push(numero_utente)
+        console.log(arr_utente);
+    } else if ((numero_utente < 1) || (numero_utente > 100)){
+        console.log('Inserisci un numero corretto');
+    }
+} while (!arr.includes(numero_utente) || arr_utente.length > 84);
+
+if (arr_utente.length == 84) {
+    console.log('Hai vinto!');
+} else {
+    console.log('hai perso!');
+}
